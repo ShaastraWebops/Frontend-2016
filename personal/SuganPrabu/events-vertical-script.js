ae414faf8b33046eb0aae95395651c5f990a7997
SuganPrabu <suganprabu1996@gmail.com>
2015-06-10 02:44:45 +0530

    Added content for the reverse side of event tiles

--- personal/SuganPrabu/events-vertical-script.js
+++ personal/SuganPrabu/events-vertical-script.js
@@ -6,33 +6,55 @@ app.controller('eventsVerticalCtrl',function($scope){
 		{row:[
 		{
 			name:'Big Data Challenge',
-			date:'3/Jan, 2015'
+			date:'3/Jan, 2015',
+			imageURL:'',
+			eventURL:''
 		},
 		{
 			name:'Crypto',
-			date:'3/Jan, 2015'
+			date:'3/Jan, 2015',
+			imageURL:'',
+			eventURL:''
 		},
 		{
 			name:'Hackathon',
-			date:'3/Jan, 2015'
+			date:'3/Jan, 2015',
+			imageURL:'',
+			eventURL:''
 		}
 		]
 		},
 		{row:[
 		{
 			name:'Hackfest',
-			date:'3/Jan, 2015'
+			date:'3/Jan, 2015',
+			imageURL:'',
+			eventURL:''
 		},
 		{
 			name:'Online Programming Contest',
-			date:'3/Jan, 2015'
+			date:'3/Jan, 2015',
+			imageURL:'',
+			eventURL:''
 		},
 		{
 			name:'Reverse Coding',
-			date:'3/Jan, 2015'
+			date:'3/Jan, 2015',
+			imageURL:'',
+			eventURL:''
 		}
 		]
 		}
 		]
 	};
+app.directive('backImg',function(){
+	return function(scope,element,attrs){
+		var url=attrs.backImg;
+		element.css({
+			'backImg':'url('+url+')',
+			'backround-size':'cover'
+		});
+	};
 });
+});
+
