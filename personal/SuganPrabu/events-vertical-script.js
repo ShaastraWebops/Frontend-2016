723b4279b72a983373fa0efe4095bce2d04f9137
SuganPrabu <suganprabu1996@gmail.com>
2015-06-10 13:06:37 +0530

    Suggested changes made

--- personal/SuganPrabu/events-vertical-script.js
+++ personal/SuganPrabu/events-vertical-script.js
@@ -3,58 +3,49 @@ app.controller('eventsVerticalCtrl',function($scope){
 	$scope.eventsJSON={
 		eventCategoryName:'Coding Events',
 		eventsRows:[
-		{row:[
-		{
-			name:'Big Data Challenge',
-			date:'3/Jan, 2015',
-			imageURL:'',
-			eventURL:''
-		},
-		{
-			name:'Crypto',
-			date:'3/Jan, 2015',
-			imageURL:'',
-			eventURL:''
-		},
-		{
-			name:'Hackathon',
-			date:'3/Jan, 2015',
-			imageURL:'',
-			eventURL:''
-		}
-		]
-		},
-		{row:[
-		{
-			name:'Hackfest',
-			date:'3/Jan, 2015',
-			imageURL:'',
-			eventURL:''
-		},
-		{
-			name:'Online Programming Contest',
-			date:'3/Jan, 2015',
-			imageURL:'',
-			eventURL:''
-		},
-		{
-			name:'Reverse Coding',
-			date:'3/Jan, 2015',
-			imageURL:'',
-			eventURL:''
-		}
-		]
-		}
-		]
-	};
-app.directive('backImg',function(){
-	return function(scope,element,attrs){
-		var url=attrs.backImg;
-		element.css({
-			'backImg':'url('+url+')',
-			'backround-size':'cover'
-		});
-	};
-});
+  	  {row:[
+				{
+		  		name: 'Big Data Challenge',
+		  		date: '3/Jan, 2015',
+		  		imageURL: 'http://upload.wikimedia.org/wikipedia/en/thumb/1/1c/IIT_Kharagpur_Logo.svg/268px-IIT_Kharagpur_Logo.svg.png',
+		  		eventURL: ''
+				},
+				{
+		  		name: 'Crypto',
+		  		date: '3/Jan, 2015',
+		  		imageURL: 'http://www.sit.iitd.ac.in/site-assets/images/iitd_logo.png',
+		  		eventURL: ''
+				},
+				{
+		  		name: 'Hackathon',
+		  		date: '3/Jan, 2015',
+		  		imageURL: 'http://www.iitb.ac.in/sites/all/themes/touchm/logo.png',
+		  		eventURL :''
+				}
+				]
+	  	},
+	  	{row:[
+				{
+		  		name: 'Hackfest',
+		  		date: '3/Jan, 2015',
+		  		imageURL: 'http://upload.wikimedia.org/wikipedia/en/thumb/6/69/IIT_Madras_Logo.svg/1024px-IIT_Madras_Logo.svg.png',
+		  		eventURL: ''
+				},
+				{
+		  		name: 'Online Programming Contest',
+		  		date: '3/Jan, 2015',
+		  		imageURL: 'http://www.iitk.ac.in/rfidlabs/images/logo6.png',
+		  		eventURL: ''
+				},
+				{
+		  		name: 'Reverse Coding',
+		  		date: '3/Jan, 2015',
+		  		imageURL: 'http://upload.wikimedia.org/wikipedia/en/thumb/6/69/IIT_Madras_Logo.svg/1024px-IIT_Madras_Logo.svg.png',
+		  		eventURL: ''
+	    	}
+				]
+	  	}
+			]
+  };
 });
 
