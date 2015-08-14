c241604a8e9e1e5541a90cbe6203a5b4de8d86c3
ojasvita <ojasvitawalgad@gmail.com>
2015-08-15 02:20:38 +0530

    updated eventlist page (scroll)

--- personal/ankan/app1.js
+++ personal/ankan/app1.js
@@ -5,36 +5,36 @@ app.controller('eventsctrl',function(){
 });
 var lists=[
 {
-		    'name': 'Aerofest',
-		    'imgUrl': 'images/aerofest.png'
+		    name: "Aerofest",
+		    imgUrl: "images/aerofest.png",
 		},
 		{
-		    'name': 'B-events',
-		    'imgUrl': 'images/bevents.png'
+		    name: "B-events",
+		    imgUrl: "images/bevents.png",
 		},
 		{
-		    'name': 'Coding',
-		    'imgUrl': 'images/coding.png'
+		    name: "Coding",
+		    imgUrl: "images/coding.png",
 		},
 		{
-		    'name': 'Design n Build',
-		    'imgUrl': 'images/design and bulid.png'
+		    name: "Design And Build",
+		    imgUrl: "images/design and build.png",
 		},
 		{
-		    'name': 'Department Flagship',
-		    'imgUrl': 'images/aerofest.png'
+		    name: "Department Flagship",
+		    imgUrl: "images/aerofest.png",
 		},
 		{
-		    'name': 'Electronic Fest',
-		    'imgUrl': 'images/aerofest.png'
+		    name: "Electronic Fest",
+		    imgUrl: "images/aerofest.png",
 		},
 		{
-		    'name': 'Involve',
-		    'imgUrl': 'images/aerofest.png'
+		    name: "Involve",
+		    imgUrl: "images/aerofest.png",
 		},
 		{
-		    'name': 'Quizzing',
-		    'imgUrl': 'images/aerofest.png'
+		    name: "Quizzing",
+		    imgUrl: "images/aerofest.png",
 		},
 		{
 		    'name': 'Spotlight',
