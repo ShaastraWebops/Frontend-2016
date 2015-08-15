04f85fd25c618b5a95bafe0d65de451f3e089846
AnandJM <mysterjoseph14@gmail.com>
2015-08-15 12:48:51 +0530

    Hamburger
    hamburger and scroll added

--- personal/anandjm/app.js
+++ personal/anandjm/app.js
@@ -9,16 +9,43 @@ app.directive('down',
         return {
             restrict: 'C',
             link: function() {
-          $('.down a').click(function(){
+          $('.down .d').click(function(){
             $(this).next('.list').toggleClass("clicked");
             if($(this).next('.list').hasClass("clicked"))
             {
-              $(this).parent().css("padding-bottom","0");
+              $(this).parent().css("padding-bottom","0em");
             }
-            else
-            {
-              $(this).parent().css("padding-bottom","0.5em");
-            }
+            
+          });
+                               }
+                               };
+});
+app.directive('o',
+  function() {
+        return {
+            restrict: 'C',
+            link: function() {
+          $('.o').click(function(){
+            $('.o').addClass("none");
+            $('.o').removeClass("inblock");
+            $('.menu').addClass("mnopen");
+            $('.c').addClass("block");
+          });
+                               }
+                               };
+});
+
+app.directive('c',
+  function() {
+        return {
+            restrict: 'C',
+            link: function() {
+          $('.c').click(function(){
+            $('.down .list').removeClass("clicked");
+            $('.menu').removeClass("mnopen");
+            $('.c').removeClass("block");
+            $('.o').removeClass("none");
+            $('.o').addClass("inblock");
           });
                                }
                                };
