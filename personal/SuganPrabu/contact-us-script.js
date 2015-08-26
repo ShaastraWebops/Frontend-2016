ea2485a31f622e55cab89a6204acb4ca346abc1e
ojasvita <ojasvitawalgad@gmail.com>
2015-08-27 01:14:54 +0530

    demo.html file with scroll updates

--- personal/SuganPrabu/contact-us-script.js
+++ personal/SuganPrabu/contact-us-script.js
@@ -1,20 +1,104 @@
-var app = angular.module('contactUsApp',[]);
-app.controller('contactUsCtrl', ['$scope', 
-	function($scope){
+var app = angular.module('contactUsApp',['ui.bootstrap']);
+app.controller('contactUsCtrl', ['$scope', '$modal',
+	function($scope, $modal){
 		$scope.departmentsJSON = {
 			departments:[
-				{name: "Co-Curricular Affairs Secretary"}, 
-				{name: "Concept & Design"},
-				{name: "Events"},
-				{name: "Finance & Sales"},
-				{name: "Facilities & Catering"},
-				{name: "Sponsorship & Public Relations"},
-				{name: "Shows"},
-				{name: "Marketing & Student Relations"},
-				{name: "Quality Management Systems"},
-				{name: "Evolve"},
-				{name: "Envisage"},
-				{name: "Web Operations"}
+				{id: 0,
+				 name: "Co-Curricular Affairs Secretary",
+				 contacts: [
+				 {
+				 	personName: "SIDDHARTH DIALANI",
+				 	phone: "9884308764",
+				 	mail: "siddharth@shaastra.org"
+				 }]}, 
+				{id: 1,
+			     name: "Concept & Design",
+			 	 contacts: [
+				 {
+				 	personName: "ASHAY MAKIM",
+				 	phone: "9043812193",
+				 	mail: "ashay@shaastra.org"
+				 }]},
+				{id: 2, 
+				 name: "Events",
+				 contacts: [
+				 {
+				 	personName: "SHIVAPRASAD TR",
+				 	phone: "9176014050",
+				 	mail: "shiva@shaastra.org"
+				 }]},
+				{id: 3,
+				 name: "Finance & Sales",
+				 contacts: [
+				 {
+				 	personName: "VINAY CHITTETI",
+				 	phone: "7845604564",
+				 	mail: "vinay@shaastra.org"
+				 }]},
+				{id: 4,
+				 name: "Facilities & Catering",
+				 contacts: [
+				 {
+				 	personName: "ABHISHEK SHARMA",
+				 	phone: "9940116444",
+				 	mail: "abhishek@shaastra.org"
+				 }]},
+				{id: 5,
+				 name: "Sponsorship & Public Relations",
+				 contacts: [
+				 {
+				 	personName: "SHUBHAM JAIN",
+				 	phone: "9962035992",
+				 	mail: "shubham@shaastra.org"
+				 }]},
+				{id: 6,
+				 name: "Shows",
+				 contacts: [
+				 {
+				 	personName: "",
+				 	phone: "",
+				 	mail: ""
+				 }]},
+				{id: 7,
+				 name: "Marketing & Student Relations",
+				 contacts: [
+				 {
+				 	personName: "NAMIT CHAUDHARY",
+				 	phone: "9962878704",
+				 	mail: "namit@shaastra.org"
+				 }]},
+				{id: 8,
+				 name: "Quality Management Systems",
+				 contacts: [
+				 {
+				 	personName: "DHRUVA KATREKAR",
+				 	phone: "9962303153",
+				 	mail: "dhruva@shaastra.org"
+				 }]},
+				{id: 9,
+				 name: "Evolve",
+				 contacts: [
+				 {
+				 	personName: "",
+				 	phone: "",
+				 	mail: ""
+				 }]},
+				{id: 10,
+				 name: "Envisage",
+				 contacts: [
+				 {
+				 	personName: "SANDEEP KRISHNA",
+				 	phone: "9789808683",
+				 	mail: "sandeep@shaastra.org"
+				 }]},
+				{id: 11,
+				 name: "Web Operations",
+				 contacts: [
+				 {
+				 	personName: "",
+				 	phone: "",
+				 	mail: ""
+				 }]}
 				]
 			};
 		$scope.initialize = function(){
@@ -26,6 +110,29 @@ app.controller('contactUsCtrl', ['$scope',
 			};
 			var map = new google.maps.Map(ele, options);
 		};
+		$scope.displayModal = function(department){
+		console.log("Clicked");
+		var modalInstance = $modal.open({
+	  		templateUrl: 'myModalContent.html',
+      		controller: 'ModalInstanceCtrl',
+      		resolve: {
+        		department: function () {
+          			return department;
+        		}
+      		}
+		});
+		modalInstance.result.then(function (selectedItem) {
+    	  $scope.selected = selectedItem;
+    	}, function () {
+      	});
+	}
 	}]);
-<!-- Script for google maps -->
-
+app.controller("ModalInstanceCtrl", function($scope, $modalInstance, department){
+	$scope.department = department;
+	$scope.close = function(){
+		$modalInstance.dismiss('cancel');
+	}
+});
+$('.rhombus').hover(function(){
+	//$('.rhombus-front').animate({opacity : })
+});
