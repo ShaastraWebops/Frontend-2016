62306f3ea9c0819930ecb7084faf18a7d57f9069
minu-aswanth <minuapple@gmail.com>
2015-09-13 23:34:04 +0530

    menu modified

--- personal/Anirudh/spons-brochure/menu.js
+++ personal/Anirudh/spons-brochure/menu.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 var count=0;
 $("#closemenu").click(function(){
   if(count==1){
-    $("ul").animate({right: '-200px'});
+    $("#menu-container").animate({right: '-200px'});
     $("#closemenu").animate({right: '30px'});
     $("#closeiconcontainer").css("display","none");
     $("#closefirstline").css("display","none");
@@ -14,7 +14,7 @@ $("#closemenu").click(function(){
     count=count-1;
   }
   else{
-    $("ul").animate({right: '0px'});
+    $("#menu-container").animate({right: '0px'});
     $("#closemenu").animate({right: '120px'});
     $("#closeiconcontainer").css("display","block");
     $("#closefirstline").css("display","block");
