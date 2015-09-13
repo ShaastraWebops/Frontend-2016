4075e184105362a72a87cab2768662dc4c3ec5a7
minu-aswanth <minuapple@gmail.com>
2015-09-13 10:49:02 +0530

    added separate menu files

---
+++ personal/Anirudh/menu.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+var count=0;
+$("#closemenu").click(function(){
+  if(count==1){
+    $("ul").animate({right: '-200px'});
+    $("#closemenu").animate({right: '30px'});
+    $("#closeiconcontainer").css("display","none");
+    $("#closefirstline").css("display","none");
+    $("#closesecondline").css("display","none");
+    $("#firstline").css("display","block");
+    $("#secondline").css("display","block");
+    $("#thirdline").css("display","block");
+    $("#fourthline").css("display","block");
+    count=count-1;
+  }
+  else{
+    $("ul").animate({right: '0px'});
+    $("#closemenu").animate({right: '120px'});
+    $("#closeiconcontainer").css("display","block");
+    $("#closefirstline").css("display","block");
+    $("#closesecondline").css("display","block");
+    $("#firstline").css("display","none");
+    $("#secondline").css("display","none");
+    $("#thirdline").css("display","none");
+    $("#fourthline").css("display","none");
+    count=count+1;
+  }
+});
+});
