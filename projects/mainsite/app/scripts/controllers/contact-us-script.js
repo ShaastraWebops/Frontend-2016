427e8578a53e92c16bcf594f464157b8ca48e1dd
susmitha <sush02a@gmail.com>
2015-09-18 23:55:26 +0530

    Update contact-us-script.js

--- projects/mainsite/app/scripts/controllers/contact-us-script.js
+++ projects/mainsite/app/scripts/controllers/contact-us-script.js
@@ -151,14 +151,19 @@ app.controller('contactUsCtrl', ['$scope', '$modal',
 				 }]}
 				]
 			};
-		$scope.initialize = function(){
-			var ele = document.querySelector("#map");
-			var options = {
-				center: new google.maps.LatLng(12.9915, 80.2336),
-				zoom: 15,
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-			};
-			var map = new google.maps.Map(ele, options);
+		$scope.init_map = function(){
+		// 	var ele = document.querySelector("#map");
+		// 	var options = {
+		// 		center: new google.maps.LatLng(12.9915, 80.2336),
+		// 		zoom: 15,
+		// 		mapTypeId: google.maps.MapTypeId.ROADMAP
+		// 	};
+		// 	var map = new google.maps.Map(ele, options);
+		var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 12.9915, lng: 80.2336},
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
 		};
 		$scope.displayModal = function(department){
 		console.log("Clicked");
