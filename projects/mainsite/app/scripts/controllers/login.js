a97708d3426d0920eb3d140a49748a7cf0a19512
chinnichaitanya <chchaitanya95@gmail.com>
2015-09-29 01:21:50 +0530

    login working

--- projects/mainsite/app/scripts/controllers/login.js
+++ projects/mainsite/app/scripts/controllers/login.js
@@ -1,16 +1,20 @@
 'use strict';
 angular.module('shaastra2016App')
-  .controller('loginCtrl', function ($scope, Auth, $location) {
+  .controller('loginCtrl', function ($scope, Auth, $location, $http) {
     $scope.UserName = "";
     $scope.Password = "";
 
     Auth.login({
-      email: 'abc@abc.c',
-      password: 'abcabc'
+      email: 'core1@core.com',
+      password: 'core'
     })
     .then( function() {
       // Logged in, redirect to home
-      $location.path('#/dashboard');
+      // $location.path('/dashboard');
+      $http.get('http://0.0.0.0:8001/api/users/me')
+        .then(function (response) {
+          console.log(response.data);
+        });
     })
     .catch( function (err) {
       console.log(err);
