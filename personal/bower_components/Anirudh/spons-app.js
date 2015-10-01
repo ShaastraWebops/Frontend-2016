7548614996fd6d23c835b43510b15d4fd8bffbe0
susmitha <sush02a@gmail.com>
2015-10-02 01:49:18 +0530

    'Loading...'

---
+++ personal/bower_components/Anirudh/spons-app.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+var tenDays = 86400000*10;
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, '/spons-brochure', 'spons-rep.html'));
+});
+
+app.get('/avenues', function (req, res) {
+	res.sendFile(path.join(__dirname, '/spons-brochure', 'spons-avenues.html'));
+});
+
+app.get('/publicity', function (req, res) {
+	res.sendFile(path.join(__dirname, '/spons-brochure', 'spons-publicity.html'));
+});
+
+app.get('/sponsors-quotes', function (req, res) {
+	res.sendFile(path.join(__dirname, '/spons-brochure', 'spons-sponsors-quotes.html'));
+});
+
+app.get('/participants-quotes', function (req, res) {
+	res.sendFile(path.join(__dirname, '/spons-brochure', 'spons-participants-quotes.html'));
+});
+
+app.get('/social', function (req, res) {
+	res.sendFile(path.join(__dirname, '/spons-brochure', 'spons-social.html'));
+});
+
+app.get('/why-shaastra', function (req, res) {
+	res.sendFile(path.join(__dirname, '/spons-brochure', 'spons-why.html'));
+});
+
+app.get('/workshops', function (req, res) {
+	res.sendFile(path.join(__dirname, '/spons-brochure', 'spons-workshops.html'));
+});
+
+app.get('/aerofest', function (req, res) {
+	res.sendFile(path.join(__dirname, '/spons-brochure', 'spons-events-aerofest.html'));
+});
+
+app.get('/b-events', function (req, res) {
+	res.sendFile(path.join(__dirname, '/spons-brochure', 'spons-events-B-events.html'));
+});
+
+app.get('/department-flagship', function (req, res) {
+	res.sendFile(path.join(__dirname, '/spons-brochure', 'spons-events-department-flagship.html'));
+});
+
+app.get('/design-and-build', function (req, res) {
+	res.sendFile(path.join(__dirname, '/spons-brochure', 'spons-events-design-and-build.html'));
+});
+
+app.get('/electronics-fest', function (req, res) {
+	res.sendFile(path.join(__dirname, '/spons-brochure', 'spons-events-electronics-fest.html'));
+});
+
+app.get('/involve', function (req, res) {
+	res.sendFile(path.join(__dirname, '/spons-brochure', 'spons-events-involve.html'));
+});
+
+app.get('/quizzing', function (req, res) {
+	res.sendFile(path.join(__dirname, '/spons-brochure', 'spons-events-quizzing.html'));
+});
+
+app.get('/research', function (req, res) {
+	res.sendFile(path.join(__dirname, '/spons-brochure', 'spons-events-research.html'));
+});
+
+// app.get('/*', function (req, res) {
+// 	res.sendFile(path.join(__dirname, '/spons-brochure', 'spons-404.html'));
+// });
+  // app.route('/*')
+  //   .get(function (req, res) {
+  //     // console.log(app.get('appPath'));
+  //     // res.sendfile(app.get('appPath') + '/index.html');
+  //     res.sendFile(path.join(__dirname, '/spons-brochure', 'spons-error.html'));
+  //   });
+
+app.use(express.static(__dirname + '/spons-brochure', { maxAge: tenDays }));
+
+app.listen(8081);
---
+++ personal/bower_components/Anirudh/spons-brochure/menu.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+var count=0;
+$("#closemenu").click(function(){
+  if(count==1){
+    $("#menu-container").animate({right: '-200px', opacity: '0'});
+    $("#closemenu").animate({right: '30px'});
+    $("#closeiconcontainer").css("display","none");
+    $("#closefirstline").css("display","none");
+    $("#closesecondline").css("display","none");
+    $("#firstline").css("display","block");
+    $("#secondline").css("display","block");
+    $("#thirdline").css("display","block");
+    $("#fourthline").css("display","block");
+    count=count-1;
+  }
+  else{
+    $("#menu-container").animate({right: '0px', opacity: '1'});
+    $("#closemenu").animate({right: '120px'});
+    $("#closeiconcontainer").css("display","block");
+    $("#closefirstline").css("display","block");
+    $("#closesecondline").css("display","block");
+    $("#firstline").css("display","none");
+    $("#secondline").css("display","none");
+    $("#thirdline").css("display","none");
+    $("#fourthline").css("display","none");
+    count=count+1;
+  }
+});
+});
