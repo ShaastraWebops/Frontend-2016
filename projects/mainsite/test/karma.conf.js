afb64c764b9ffb5212671469099b862c7651a1f7
chinnichaitanya <chchaitanya95@gmail.com>
2015-10-05 16:25:23 +0530

    fix bower json

--- projects/mainsite/test/karma.conf.js
+++ projects/mainsite/test/karma.conf.js
@@ -27,6 +27,7 @@ module.exports = function(config) {
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/angular-cookies/angular-cookies.js',
+      'bower_components/angular-local-storage/dist/angular-local-storage.js',
       'bower_components/angular-messages/angular-messages.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
