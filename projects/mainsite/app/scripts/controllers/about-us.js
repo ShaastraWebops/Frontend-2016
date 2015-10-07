2906006e90f82a5245271be84a8819a845a79575
chinnichaitanya <chchaitanya95@gmail.com>
2015-10-07 22:13:05 +0530

    add about us page

--- projects/mainsite/app/scripts/controllers/about-us.js
+++ projects/mainsite/app/scripts/controllers/about-us.js
@@ -3,6 +3,9 @@
 angular.module('shaastra2016App')
   .controller('aboutUsCtrl', function ($scope) {
 
+    var html = angular.element(document.getElementById('body'));
+    html.css({'overflow-y': 'auto'});
+
 		var backButton = $('#back-button');
 		backButton.attr('link', '/');
 
