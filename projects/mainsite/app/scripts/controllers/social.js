97d414e23837a333970ea74619ac0b25d691e240
chinnichaitanya <chchaitanya95@gmail.com>
2015-10-08 21:54:25 +0530

    fix bugs

--- projects/mainsite/app/scripts/controllers/social.js
+++ projects/mainsite/app/scripts/controllers/social.js
@@ -3,6 +3,9 @@
 angular.module('shaastra2016App')
   .controller('socialCtrl', function ($scope) {
 
+    var html = angular.element(document.getElementById('body'));
+    html.css({'overflow': 'auto'});
+
 		var backButton = $('#back-button');
 		backButton.attr('link', '/');
 
