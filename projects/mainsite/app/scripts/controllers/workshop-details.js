b5292cfb39a8921a15f4971a1f06f4b725509831
chinnichaitanya <chchaitanya95@gmail.com>
2015-10-26 15:49:36 +0530

    add marquee to workshops

--- projects/mainsite/app/scripts/controllers/workshop-details.js
+++ projects/mainsite/app/scripts/controllers/workshop-details.js
@@ -9,6 +9,7 @@ angular.module('shaastra2016App')
     var html = angular.element(document.getElementById('body'));
     html.css({
       'overflow-y': 'auto',
+      'overflow-x': 'hidden', 
       'background-color': '#f3f3f3'
     });
     
@@ -19,6 +20,7 @@ angular.module('shaastra2016App')
       return converter.makeHtml(b);
     };
     
+    var notifs = [];
     $scope.eve = [];
     $http.get('http://shaastra.org:8001/api/events/showWeb/' + $routeParams.workshopId)
       .then(function (response) {
@@ -28,6 +30,7 @@ angular.module('shaastra2016App')
           return 0;
         });
         var numAssignees = response.data.assignees.length;
+        var numNotifs = response.data.marqueeNotifs.length;
         var contact = {
           'name': 'Contact Details',
           'info': '<p><b>For further details, please contact,</b></p>'
@@ -35,6 +38,15 @@ angular.module('shaastra2016App')
         for(var i=0; i<numAssignees; i++) {
           contact.info += '<p>' + response.data.assignees[i].name + ' - ' + response.data.assignees[i].phoneNumber + '</p>';
         }
+        // for marquee notifs start
+        for(i=0; i<numNotifs; i++) {
+          notifs.push(response.data.marqueeNotifs[i].info);
+        }
+        var notifHtml = notifs.join(' &emsp;&emsp;&emsp;&emsp;&emsp;<span class="dot"></span>&emsp;');
+        if(numNotifs > 0) {
+          document.getElementById("markin").innerHTML ='<span class="dot"></span>&emsp;' + notifHtml;
+        }
+        // for marquee notifs end
         response.data.eventTabs.push(contact);
         $scope.message = 'Stay tuned for Updates!';
         $scope.eve = response.data;
