c2ede94d36181c2df56c113e84c7f1471ffaed7e
chinnichaitanya <chchaitanya95@gmail.com>
2015-12-23 17:36:02 +0530

    updates in shows page

--- projects/mainsite/app/scripts/controllers/shows-and-exhibitions.js
+++ projects/mainsite/app/scripts/controllers/shows-and-exhibitions.js
@@ -12,7 +12,7 @@ angular.module('shaastra2016App')
       'background-color': '#f3f3f3'
     });
 
-    $scope.showsTabs = ["Tech and Innovation Fair", "Past Performances"];
+    $scope.showsTabs = ["Tech and Innovation Fair 2016", "Past Performances"];
     $scope.tab = 0;
 
     $scope.changeTab = function (index) {
