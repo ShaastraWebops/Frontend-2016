9f6ef81e094cfee0609f696da5dbe48392d4af13
chinnichaitanya <chchaitanya95@gmail.com>
2015-12-25 19:33:05 +0530

    Add fellowship-registration link in sis-fellowship page

--- projects/mainsite/app/scripts/controllers/sis-fellowship.js
+++ projects/mainsite/app/scripts/controllers/sis-fellowship.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('shaastra2016App')
-  .controller('sisFellowshipCtrl', function ($scope, $location, $anchorScroll) {
+  .controller('sisFellowshipCtrl', function ($scope, $location, $anchorScroll, Auth) {
 
     $scope.pageClass = 'page-sisFellowship';
 
@@ -13,6 +13,29 @@ angular.module('shaastra2016App')
 
 		var backButton = $('#back-button');
 		backButton.attr('link', '/');
+
+    $scope.logged = false;
+    Auth.isLoggedInAsync(function (loggedIn) {
+      $scope.user = Auth.getCurrentUser();
+      $scope.logged = loggedIn;
+    });
+
+    $scope.sisFellowshipWorking = false;
+    $scope.sisFellowship = function() {
+      $scope.sisFellowshipWorking = true;
+      $http.post('http://shaastra.org:8001/api/users/sisFellowship')
+        .then(function (response) {
+          if(response.status === 200) {
+            $scope.user.interestedInShaastraFellowship = true;
+            $scope.sisFellowshipWorking = false;
+            alert('Successfully registered for Shaastra Fellowship');
+          } else {
+            $scope.user.interestedInShaastraFellowship = false;
+            $scope.sisFellowshipWorking = false;
+            alert('Some error has occurred');
+          }
+        });
+    };
 
     $scope.scrollDown = function (element) {
       var temp = $location.hash();
