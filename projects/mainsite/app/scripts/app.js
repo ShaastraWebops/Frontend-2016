a8a92a1959799f4f6ee8188defd6e664748af396
chinnichaitanya <chchaitanya95@gmail.com>
2016-01-10 00:33:43 +0530

    corrections in ogp and add fb app id

--- projects/mainsite/app/scripts/app.js
+++ projects/mainsite/app/scripts/app.js
@@ -320,6 +320,13 @@ angular
     ga('send', 'pageview', { page: $location.url() });
 
     // For Facebook
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '1630409340524916',
+        xfbml      : true,
+        version    : 'v2.5'
+      });
+    };
     (function(d, s, id){
        var js, fjs = d.getElementsByTagName(s)[0];
        if (d.getElementById(id)) {return;}
