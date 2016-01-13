064c7803c413abf0e503b86fabdf28c94c8c7188
chinnichaitanya <chchaitanya95@gmail.com>
2016-01-13 17:51:02 +0530

    updates in cost calculator

--- projects/mainsite/app/scripts/controllers/accommodation-calculator.js
+++ projects/mainsite/app/scripts/controllers/accommodation-calculator.js
@@ -14,7 +14,7 @@ angular.module('shaastra2016App')
 		var backButton = $('#back-button');
 		backButton.attr('link', '/');
 
-    $scope.costPerDay = 100;
+    $scope.costPerDay = 300;
     $scope.totalCost = 0;
     $scope.accomNumber = 0;
     $scope.members = [];
@@ -23,7 +23,7 @@ angular.module('shaastra2016App')
     $scope.firstDate = [];
     $scope.secondDate = [];
     $scope.validation = [];
-    $scope.selectedTimes = ["00:00", "00:30", "01:00", "01:30", "02:00", "02:30", "03:00", "03:30", "04:00", "04:30", "05:00", "05:30", "06:00", "06:30", "07:00", "07:30", "08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00", "22:30", "23:00", "23:30"];
+    $scope.selectedTimes = ["00:30", "01:00", "01:30", "02:00", "02:30", "03:00", "03:30", "04:00", "04:30", "05:00", "05:30", "06:00", "06:30", "07:00", "07:30", "08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00", "22:30", "23:00", "23:30"];
 
     $scope.changeAccomNum = function (n) {
       $scope.totalCost = 0;
@@ -56,7 +56,7 @@ angular.module('shaastra2016App')
         $scope.totalCost = 0;
         var timeDiff = Math.abs($scope.secondDate[index] - $scope.firstDate[index]);   
         $scope.numDaysOfStay[index] = Math.ceil((timeDiff / (1000 * 3600 * 24)) + 1);
-        $scope.individualCost[index] = 100 * $scope.numDaysOfStay[index];
+        $scope.individualCost[index] = $scope.costPerDay * $scope.numDaysOfStay[index];
         for(var i=0; i<n; i++) {
           $scope.totalCost = $scope.totalCost + $scope.individualCost[i];
         }
@@ -79,7 +79,7 @@ angular.module('shaastra2016App')
         $scope.totalCost = 0;
         var timeDiff = Math.abs($scope.secondDate[index] - $scope.firstDate[index]);   
         $scope.numDaysOfStay[index] = Math.ceil((timeDiff / (1000 * 3600 * 24)) + 1);
-        $scope.individualCost[index] = 100 * $scope.numDaysOfStay[index];
+        $scope.individualCost[index] = $scope.costPerDay * $scope.numDaysOfStay[index];
         for(var i=0; i<n; i++) {
           $scope.totalCost = $scope.totalCost + $scope.individualCost[i];
         }
