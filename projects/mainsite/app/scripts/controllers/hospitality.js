06854172f71b6f78dc81c30d5d5508967678e1d6
Sriram <sriramdommeti169@gmail.com>
2016-01-15 16:37:12 +0530

    add spons logo to hospitality page

--- projects/mainsite/app/scripts/controllers/hospitality.js
+++ projects/mainsite/app/scripts/controllers/hospitality.js
@@ -15,4 +15,13 @@ angular.module('shaastra2016App')
 		backButton.attr('link', '/');
 
     $scope.message = "hello";
+
+    $scope.hospiSponsor = [{
+        image1: 'http://res.cloudinary.com/shaastra-org/image/upload/c_scale,w_300,fl_lossy,f_auto,q_80/v1452713528/sponsors/2016/Hospitality-Partner/sbi.jpg',
+        link1: 'http://www.sbi.co.in'
+      },
+      {
+        image1: 'http://res.cloudinary.com/shaastra-org/image/upload/v1452031701/sponsors/2016/Stay-Partner/stayzilla.jpg',
+        link1: 'http://stayzilla.com/'
+      }];
   });
