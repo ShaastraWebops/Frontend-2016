c41d3716f0e9981726c52edaa25a8d7c0212272c
chinnichaitanya <chchaitanya95@gmail.com>
2016-01-16 14:22:27 +0530

    add stream, degree, state

--- projects/mainsite/app/scripts/controllers/register.js
+++ projects/mainsite/app/scripts/controllers/register.js
@@ -16,6 +16,41 @@ angular.module('shaastra2016App')
 
     $scope.addCollegeMessage = '';
 
+    $scope.streams = [
+      'Aeronautical / Aerospace Engineering', 
+      'Chemical / Petroleum Engineering',
+      'Civil Engineering',
+      'Commerce',
+      'Computer Science Engineering',
+      'Electrical Engineering / Electronics & Telecommunication',
+      'Humanities',
+      'Information Technology / Information Science',
+      'Mechanical Engineering',
+      'Metallurgical Engineering',
+      'Pure Sciences',
+      'Others'
+    ];
+    $scope.states = [
+      'Andhra Pradesh',
+      'Delhi',
+      'Goa',
+      'Karnataka',
+      'Kerala',
+      'Madhya Pradesh',
+      'Maharashtra',
+      'Pondicherry',
+      'Tamil Nadu',
+      'Telangana',
+      'Other State',
+      'International'
+    ];
+    $scope.degrees = [
+      'Bachelors',
+      'Masters',
+      'PhD',
+      'None'
+    ];
+
     var n = 0;
     $scope.name = "";
     $scope.secondName = "";
@@ -28,6 +63,9 @@ angular.module('shaastra2016App')
     $scope.college = {};
     $scope.city = "";
     $scope.collegeSelected = '';
+    $scope.state = '';
+    $scope.degree = '';
+    $scope.stream = '';
     $scope.collegeSuggestions = [];
     $scope.existingCollege = 1;
     $scope.schoolStudent = false;
@@ -91,8 +129,11 @@ angular.module('shaastra2016App')
     $scope.registerUser = function(){
       $scope.submitted = true;
       if($scope.form.$valid && ($scope.password === $scope.confirmPassword)) {
-      $scope.userRegisterMessage = " -- Working...";
+        $scope.userRegisterMessage = " -- Working...";
         $scope.disableRegister = true;
+        alert($scope.stream);
+        alert($scope.state);
+        alert($scope.degree);
         Auth.createUser({
           name: $scope.name,
           secondName: $scope.secondName,
@@ -101,6 +142,9 @@ angular.module('shaastra2016App')
           phoneNumber: $scope.phoneNumber,
           age: $scope.age,
           city: $scope.city,
+          state: $scope.state,
+          stream: $scope.stream,
+          degree: $scope.degree,
           gender: $scope.gender,
           wantAccomodation: $scope.wantAccomodation,
           college: $scope.collegeSelected,
